64bcf2cf9a7486ceb5d119038b29f1e1f341ba64
kennard ng pool hua <kennard.ng.pool.hua@gmail.com>
2021-01-08 21:38:47 +0800

    hotfix discord bot

--- notifyhub/bots/discordbot.py
+++ notifyhub/bots/discordbot.py
@@ -1,8 +1,8 @@
 import discord
+import asyncio
 from abc import abstractmethod
 
 
-# Taken from https://github.com/Querela/discord-notifier-bot
 class SingleTaskClient(discord.Client):
     @abstractmethod
     async def execute_task(self):
@@ -10,7 +10,7 @@ class SingleTaskClient(discord.Client):
 
     async def on_ready(self):
         await self.execute_task()
-        await self.close()
+        await self.logout()
 
 
 class SendMessageClient(SingleTaskClient):
@@ -24,17 +24,24 @@ class SendMessageClient(SingleTaskClient):
         await channel.send(self.message)
 
 
-def send_message(bot_token: str, chat_id: int, message: str):
+async def _send_message(bot_token: str, chat_id: int, message: str):
     client = SendMessageClient(message=message, channel_id=chat_id)
-    client.run(bot_token)
+    await client.login(token=bot_token)
+    await client.connect()
+
+
+def send_message(bot_token: str, chat_id: int, message: str):
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(_send_message(bot_token=bot_token, chat_id=chat_id, message=message))
 
 
 def main():
     import json
-    fp = '/home/kennardng/projects/notifyhub/configs/discord.json'
+    fp = '/home/kennardngpoolhua/projects/notifyhub/configs/discord.json'
     with open(fp, 'r') as f:
         config = json.load(f)
-    send_message(bot_token=config['bot-token'], chat_id=int(config['channel-id']), message='goodbye')
+    send_message(bot_token=config['bot-token'], chat_id=int(config['chat-id']), message='gb0')
+    send_message(bot_token=config['bot-token'], chat_id=int(config['chat-id']), message='gb1')
 
 
 if __name__ == '__main__':
